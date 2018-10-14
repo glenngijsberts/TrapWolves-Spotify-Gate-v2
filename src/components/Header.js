b9ff49d50f9220bb81fbf4b7f11d50f87c969e28
Glenn Gijsberts <glenn@sqits.nl>
2018-10-14 16:51:27 +0200

    Created a header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Logo from 'assets/img/logo_trapwolves.png'
 
 const Header = () => (
-    <h2>TrapWolves Spotify</h2>
+    <header>
+        <img src={Logo} alt="TrapWolves logo" />
+        <h1>TrapWolves Giveaway</h1>
+        <h2>Enter to win a free hoodie</h2>
+    </header>
 )
 
 export default Header
